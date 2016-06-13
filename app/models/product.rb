982fafcdadda724b17e2e40f1827027e59e8e53b
Alyson Takai <shtakai@gmail.com>
2016-06-12 22:39:05 -0700

    added validation product.price > 0

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   validates :name, :description, :pricing, presence: true
   validates :name, :description, length: { minimum: 5 }
+  validates :pricing, numericality: {greater_than: 0.00}
 
   belongs_to :category
 end
